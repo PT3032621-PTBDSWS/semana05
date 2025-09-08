a6ca1b678a584ca8ac23226f82b1740a0aa94ce1
Gustavo Maximo Da Silva <gmaximodasilva@gmail.com>
2025-09-07 22:12:01 -0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -4,9 +4,12 @@ from wtforms.validators import DataRequired, Length
 
 
 class NameForm(FlaskForm):
-name = StringField(
-"Your name",
-validators=[DataRequired(message="Digite um nome."), Length(max=50)],
-render_kw={"placeholder": "Type your name"}
-)
-submit = SubmitField("Send")
+    name = StringField(
+        "Your name",
+        validators=[
+            DataRequired(message="Digite um nome."),
+            Length(max=50)
+        ],
+        render_kw={"placeholder": "Type your name"}
+    )
+    submit = SubmitField("Send")
